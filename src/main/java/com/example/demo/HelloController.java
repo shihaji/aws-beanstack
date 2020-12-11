2c89b2ecf78f1ac47e40c9f9cd76e8c8891282aa
shihaji <shihaji@gmail.com>
2020-12-11 15:45:41 +0530

    aws beanstak example iniial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping
+	public String hello() {
+		
+		return "hello you tube";
+	}
+
+}
