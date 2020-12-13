1b0ade352d0533e6c4bf97b0886a229dae70954e
shihaji <shihaji@gmail.com>
2020-12-13 18:55:30 +0530

    controller changed

--- src/main/java/com/example/demo/AwsCiCdApplication.java
+++ src/main/java/com/example/demo/AwsCiCdApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsCiCdApplication {
+public class AwsCiCdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsCiCdApplication.class, args);
